d8ff64aa9bae43effca4be90a538204d92936892
Strol3e <alfred.frandberg@gmail.com>
2025-08-05 15:39:17 +0200

    delegate

--- src/MacOS/Delegate.zig
+++ src/MacOS/Delegate.zig
@@ -13,13 +13,17 @@ const Object = objc.Object;
 const nil: objc.c.id = @ptrFromInt(0);
 
 const AtomicBool = std.atomic.Value(bool);
+const AtomicSize = @import("FramebufferPool.zig").AtomicSize;
 
 pub fn init(running: *AtomicBool) Object {
     const class = objc.allocateClassPair(objc.getClass("NSObject"), "Delegate").?;
 
     _ = try class.addMethod("windowShouldClose:", windowShouldClose);
 
+    _ = try class.addMethod("windowDidResize:", windowDidResize);
+
     _ = objc.c.class_addIvar(class.value, "running", @sizeOf(*anyopaque), @alignOf(*anyopaque), "^v");
+    // _ = objc.c.class_addIvar(class.value, "framebuffer_size", @sizeOf(*anyopaque), @alignOf(*anyopaque), "^v");
 
     _ = objc.registerClassPair(class);
 
@@ -47,4 +51,10 @@ fn windowShouldClose(delegate: objc.c.id, _: objc.c.SEL, _: objc.c.id) callconv(
     std.debug.print("Setting running to false\n", .{});
 
     return true;
+}
+
+fn windowDidResize(_: objc.c.id, _: objc.c.SEL, notification: objc.c.id) callconv(.c) void {
+    const window = Object.fromId(notification).msgSend(Object, "object", .{});
+    const bounds = window.msgSend(Object, "contentView", .{}).msgSend(c.CGRect, "bounds", .{});
+    std.debug.print("Window did resize: {}\n", .{bounds.size.width});
 }
